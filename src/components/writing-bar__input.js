f3764ecc484b621a1e224bb208f5baae7dcdba1b
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-15 18:34:21 +0700

    add className for input

--- src/components/writing-bar__input.js
+++ src/components/writing-bar__input.js
@@ -1,6 +1,7 @@
 function WritingBarInput (props) {
   return (
     <input
+      className="writing-bar__input"
       type='text' name='message'
       placeholder='Release your emotion via a message...'
       value=''
